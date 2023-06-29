ebf0e799c092fa07b924f163cfd91e35ee3e683b
AnnaVi <annaseer@mail.ru>
2023-06-30 00:05:16 +0300

    Added click out window option js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-var modal = document.querySelector(".modal-container");
-var modalOpenButton = document.querySelector(".modal-open-button");
-var modalCloseButton = document.querySelector(".modal-close-button");
+let modal = document.querySelector(".modal-container");
+let modalWindow = document.querySelector(".modal-window");
+let modalOpenButton = document.querySelector(".modal-open-button");
+let modalCloseButton = document.querySelector(".modal-close-button");
 
 modalOpenButton.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -12,7 +13,13 @@ modalCloseButton.addEventListener("click", function() {
 });
 
 document.addEventListener('keydown', function(evt) {
-if (evt.key === "Escape") {
-  modal.classList.add("modal-container-close")
-};
+  if (evt.key === "Escape") {
+    modal.classList.add("modal-container-close")
+  };
+});
+
+modal.addEventListener("click", function (evt) {
+  if (!modalWindow.contains(evt.target)) {
+    modal.classList.add("modal-container-close");
+  }
 });
