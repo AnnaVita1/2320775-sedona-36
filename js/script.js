06222ad55058a2a43f4b76993c88811f578b2dba
AnnaVi <annaseer@mail.ru>
2023-07-02 16:19:44 +0300

    Added js for modal quantity buttons

--- js/script.js
+++ js/script.js
@@ -26,3 +26,56 @@ modal.addEventListener("click", function (evt) {
     modal.classList.add("modal-container-close");
   };
 });
+
+//Убирает количество ко клику
+document.querySelectorAll('.toggle-flex .modal-toggle-min').forEach(function(button) {
+  button.addEventListener('click', function() {
+    let input = this.parentElement.querySelector('.field-small');
+    let count = parseInt(input.value) - 1;
+    count = count < 1 ? 1 : count;
+    input.value = count;
+  });
+});
+
+//Прибавляет количество по клику
+document.querySelectorAll('.toggle-flex .modal-toggle-max').forEach(function(button) {
+  button.addEventListener('click', function() {
+    let input = this.parentElement.querySelector('.field-small');
+    let count = parseInt(input.value) + 1;
+    count = count > parseInt(input.dataset.maxCount) ? parseInt(input.dataset.maxCount) : count;
+    input.value = count;
+  });
+});
+
+//Убирает всё лишнее и невозможное при изменении поля
+document.querySelectorAll('.toggle-flex .field-small').forEach(function(input) {
+  input.addEventListener('change', function() {
+    if (this.value.match(/[^0-9]/g)) {
+      this.value = this.value.replace(/[^0-9]/g, '');
+    }
+    if (this.value == '') {
+      this.value = 1;
+    }
+    if (this.value > parseInt(this.dataset.maxCount)) {
+      this.value = parseInt(this.dataset.maxCount);
+    }
+  });
+
+  input.addEventListener('keyup', function() {
+    if (this.value.match(/[^0-9]/g)) {
+      this.value = this.value.replace(/[^0-9]/g, '');
+    }
+  });
+
+  input.addEventListener('input', function() {
+    if (this.value.match(/[^0-9]/g)) {
+      this.value = this.value.replace(/[^0-9]/g, '');
+    }
+  });
+
+  input.addEventListener('click', function() {
+    if (this.value.match(/[^0-9]/g)) {
+      this.value = this.value.replace(/[^0-9]/g, '');
+    }
+  });
+});
